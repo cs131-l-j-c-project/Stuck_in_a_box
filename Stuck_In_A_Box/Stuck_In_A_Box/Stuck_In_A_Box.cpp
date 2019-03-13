b9f1a610c596e24996a4b5854ec5c79a3923ee63
Liam Kikuchi <liamshinx@gmail.com>
2019-03-13 06:47:46 -0700

    Wowee an update

--- Stuck_In_A_Box/Stuck_In_A_Box/Stuck_In_A_Box.cpp
+++ Stuck_In_A_Box/Stuck_In_A_Box/Stuck_In_A_Box.cpp
@@ -1,50 +1,96 @@
 #include "pch.h"
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <sstream>
+#include <vector>
 #include <windows.h>
 using namespace std;
 
-void jWait() {
-	int x;
-	for (x = 0; x < 2; x++) {
-		Sleep(100);
-		cout << "" << "\n";
-		Sleep(100);
-	}
-}
-
+void lowerStr(string &s);
+void printTitle();
+vector<string> parseInput();
 
 int main()
 {
-	string curDir = "";//current direction
-	string start = ""; // user decision to start the game
+	string curDir;//current direction
+	string start; // user decision to start the game
+	vector<string> action;//Vector of words to do an action
+	COORD afterTitle;
+	afterTitle.X = 0;
+	afterTitle.Y = 9;
 
 	HANDLE hC = GetStdHandle(STD_OUTPUT_HANDLE);
 	SetConsoleTextAttribute(hC, 11);
 
-	jWait();
-	cout << " _____ _              _        _____          ___    ______           " << endl;
-	cout << "/  ___| |            | |      |_   _|        / _ \\   | ___ \\          " << endl;
-	cout << "\\ `--.| |_ _   _  ___| | __     | | _ __    / /_\\ \\  | |_/ / _____  __" << endl;
-	cout << " `--. \\ __| | | |/ __| |/ /     | || '_ \\   |  _  |  | ___ \\/ _ \\ \\/ /" << endl;
-	cout << "/\\__/ / |_| |_| | (__|   <     _| || | | |  | | | |  | |_/ / (_) >  < " << endl;
-	cout << "\\____/ \\__|\\__,_|\\___|_|\\_\\    \\___/_| |_|  \\_| |_/  \\____/ \\___/_/\\_\\" << endl;
-	jWait();
-	cout << "written By Liam, Calvin, and Jacob" << "\n";
-	jWait();
-	
-	do { 
+	printTitle();
+
+	do {
 		cout << "Would you like to begin your adventure?(yes or no)" << "\n";
 		cin >> start;
-		for (int i = 0; i < start.length(); ++i) {
-			start[i] = tolower(start[i]);
-		}
+		lowerStr(start);
 
 	} while (start != "yes" && start != "no");
-	
+
 	if (start == "no") {
 		return(0);
-	} 
-		
+	}
+
+	SetConsoleCursorPosition(hC, afterTitle);
+	cout << "                                                  " << endl
+		<< "   " << endl;
+	SetConsoleCursorPosition(hC, afterTitle);
+
+	while (true) {
+		//Put Descriptions here(Do we want to have a stutter on that text as well?)
+		action = parseInput();
+		if (action[0] == "exit")
+			break;
+		//Sort out what the user wanted
+	}
 	
-}//end of main
+	system("pause");
+}
+
+void lowerStr(string &s) {
+	for (int i = 0; i < s.length(); ++i) {
+		s[i] = tolower(s[i]);
+	}
+}
+
+void printTitle() {
+	ifstream fin;
+	char c;
+	int i = 0;
+	fin.open("Title.txt"); // This file is needed in the containing folder
+	while (fin.get(c)) {
+		if (i % 3)
+			Sleep(2);
+		cout << c;
+		i++;
+	}
+	fin.close();
+	cout << endl << endl;
+	cout << "written By Liam, Calvin, and Jacob" << endl << endl;
+}
+
+vector<string> parseInput() {
+	string s;
+	vector<string> vect = {""};
+	int word = 0;
+	cout << "What would you like to do?" << endl;
+	cin >> ws;
+	getline(cin, s);
+	lowerStr(s);
+	for (int i = 0; i < s.length(); i++) {
+		if (isspace(s[i])) {
+			word++;
+		}
+		else {
+			vect.resize(word + 1); //Makes sure vect holds only as many words as exist in the given string
+			vect[word] += s[i];//Appends the char in s[i] into vect's current index
+		}
+	}
+	return vect;
+}
+
